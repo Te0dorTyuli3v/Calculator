b17ff58be2080b940eb2e76da62f637399029522
Te0dorTyuli3v <102541717+Te0dorTyuli3v@users.noreply.github.com>
2023-10-25 23:12:48 +0300

    Add files via upload

---
+++ Calculator - Teodor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcu123lator
+{
+     internal class Calculator
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("10 to decimal {0}.", Convert.ToString(10, 2));  //от двуична в десетична бройна система, 10 става 2
+               Console.WriteLine("2 to binary {0}.", Convert.ToString(2, 10));    //от десетична в двуична бройна система, 2 става 10
+          }
+
+     }
+}
+
